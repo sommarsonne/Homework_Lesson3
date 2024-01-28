65883fe0a1995572e9e0699abf9a082beeb2e0af
sommarsonne <mumudynasty@gmail.com>
2024-01-28 21:44:38 +0300

    Четное/Нечетное

---
+++ OddNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    Console.WriteLine("Введите число");
+    
+    string input = Console.ReadLine();
+    int X = Convert.ToInt32(input);
+
+    if (X % 2 == 0)
+    {
+        Console.WriteLine("Четное число");
+    }
+    else
+    {
+        Console.WriteLine("Нечетное число");
+    }
+;
+}
+
