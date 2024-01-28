474a7550b83b323371aa32c870c1bae2d7f91078
sommarsonne <mumudynasty@gmail.com>
2024-01-28 21:47:14 +0300

    Цвета радуги

---
+++ RainbowNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите номер цвета");
+string number = Console.ReadLine();
+
+
+
+switch (number)
+{
+    case "1":
+        Console.WriteLine("Красный");
+        break;
+
+    case "2":
+        Console.WriteLine("Оранжевый");
+        break;
+
+    case "3":
+        Console.WriteLine("Желтый");
+        break;
+
+    case "4":
+        Console.WriteLine("Зеленый");
+        break;
+
+    case "5":
+        Console.WriteLine("Голубой");
+        break;
+
+    case "6":
+        Console.WriteLine("Синий");
+        break;
+
+    case "7":
+        Console.WriteLine("Фиолетовый");
+        break;
+
+    default:
+        Console.WriteLine("Введите число от 1 до 7");
+        break;
+}
