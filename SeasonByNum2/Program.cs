7a773a2d91700e6a0e9486724de1b5a15788f071
sommarsonne <mumudynasty@gmail.com>
2024-01-28 21:38:10 +0300

    Времена года по номерам Switch Case 2

---
+++ SeasonByNum2/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.WriteLine("Введите номер месяца");
+string? selection = Console.ReadLine();
+//int S = Convert.ToInt32(selection);
+
+
+//if (S > 12)
+//{
+//    Console.WriteLine("В году только 12 месяцев");
+//}
+
+
+switch (selection)
+{
+    case "1":
+        Console.WriteLine("Январь");
+        break;
+   
+    case "2":
+        Console.WriteLine("Февраль");
+        break;
+   
+    case "3":
+        Console.WriteLine("Март"); 
+        break;
+   
+    case "4":
+        Console.WriteLine("Апрель"); 
+        break;
+   
+    case "5":
+        Console.WriteLine("Май"); 
+        break;
+   
+    case "6":
+        Console.WriteLine("Июнь"); 
+        break;
+   
+    case "7":
+        Console.WriteLine("Июль"); 
+        break;
+   
+    case "8":
+        Console.WriteLine("Август"); 
+        break;
+   
+    case "9":
+        Console.WriteLine("Сентябрь"); 
+        break;
+   
+    case "10":
+        Console.WriteLine("Октябрь"); 
+        break;
+   
+    case "11":
+        Console.WriteLine("Ноябрь"); 
+        break;
+   
+    case "12":
+        Console.WriteLine("Декабрь"); 
+        break;
+  
+    default:
+        Console.WriteLine("Введите число от 1 до 12");
+        break;
+       
+      
+}
+
