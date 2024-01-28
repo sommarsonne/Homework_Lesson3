75147a8a9e8c3bcefd322f9414155ece86b1069f
sommarsonne <mumudynasty@gmail.com>
2024-01-28 21:46:37 +0300

    Температура
    (Тепло, Нормально, Холодно)

---
+++ Temperature/Program.cs
@@ -0,0 +1,23 @@
+﻿{
+    Console.WriteLine("Введите температуру");
+
+    string Temperature = Console.ReadLine();    
+    int T = Convert.ToInt32(Temperature);    // Так и не додумался до чего-то действительно рабочего 
+
+    if (T > -5)
+    {
+        Console.WriteLine("Тепло");
+    }
+    else
+
+    if (T <= -5 & T > -20)
+    {
+        Console.WriteLine("Нормально");
+    }
+    else
+
+    if (T <= -20)
+    {
+        Console.WriteLine("Холодно");
+    }
+}
