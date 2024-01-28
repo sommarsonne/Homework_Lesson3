a902edba2834b5ad81cb7b3efd75a3997e932acf
sommarsonne <mumudynasty@gmail.com>
2024-01-28 21:28:06 +0300

    Времена года по номерам
    Через Else If

---
+++ SeasonsElseIf/Program.cs
@@ -0,0 +1,95 @@
+﻿{
+    Console.WriteLine("Введите номер месяца от 1 до 12");
+    string input = Console.ReadLine();
+    int number = Convert.ToInt32(input);  //  System.FormatException: 'The input string 'asd' was not in a correct format.' // Не смог перебороть ввод знаков
+
+    if (number == 1)
+    {
+        Console.WriteLine("Январь");
+    }
+    else
+
+
+    if (number == 2)
+    {
+        Console.WriteLine("Февраль");
+    }
+    else
+
+
+    if (number == 3)
+    {
+        Console.WriteLine("Март");
+    }
+    else
+
+
+    if (number == 4)
+    {
+        Console.WriteLine("Апрель");
+    }
+    else
+
+
+    if (number == 5)
+    {
+        Console.WriteLine("Май");
+    }
+    else
+
+
+    if (number == 6)
+    {
+        Console.WriteLine("Июнь");
+    }
+    else
+
+
+    if (number == 7)
+    {
+        Console.WriteLine("Июль");
+    }
+    else
+
+
+    if (number == 8)
+    {
+        Console.WriteLine("Август");
+    }
+    else
+
+
+    if (number == 9)
+    {
+        Console.WriteLine("Сентябрь");
+    }
+    else
+
+
+    if (number == 10)
+    {
+        Console.WriteLine("Октябрь");
+    }
+    else
+
+
+    if (number == 11)
+    {
+        Console.WriteLine("Ноябрь");
+    }
+    else
+
+    if (number == 12)
+    {
+        Console.WriteLine("Декабрь");
+    }
+    else
+    {
+        Console.WriteLine("Введите число от 1 до 12");
+    }
+}
+
+
+
+
+     
